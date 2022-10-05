4c5e038f722d61b0473b7c801ced464f1eafa619
loynet <loynet@protonmail.com>
2022-10-05 03:17:42 +0100

    Initial changes for moderation features
    Changes the architecture by trading inheritance for composition;  Adds a method to fetch logs of a board; Flags a new bug and fixes old ones; Refactors code;  Adds initial code for moderation-related features (still work in progress);

--- main.py
+++ main.py
@@ -2,26 +2,32 @@ import os
 import sys
 
 from dotenv import load_dotenv
-from src import Bot
-from src import JSChan
 
 from rich import print
 from time import sleep
 
+from src.bot import Bot
+from src.jschan import JSChan
+from src.mod import Mod
+from src.pleroma import Pleroma
+
 load_dotenv(".env.development")
 
-jschan_website = JSChan(os.getenv("JSCHAN_WEBSITE"))
+
+def new_post_handler(post_id, board_uri, status_id):
+    mod.watch(post_id, board_uri, status_id)
+
+
+jschan = JSChan(os.getenv("JSCHAN_WEBSITE"))
+pleroma = Pleroma(os.getenv("FEDIVERSE_INSTANCE"), os.getenv("ACCESS_TOKEN"), os.getenv("ACCOUNT_ID"))
+
+bot = Bot(pleroma, jschan,
+          allowed_boards=os.getenv("BOARDS_WHITELIST").split() if "BOARDS_WHITELIST" in os.environ else None,
+          on_publish=new_post_handler)
+mod = Mod(jschan, pleroma)
 
 if __name__ == "__main__":
-    bot = Bot(
-        os.getenv("FEDIVERSE_INSTANCE"),
-        os.getenv("ACCESS_TOKEN"),
-        os.getenv("ACCOUNT_ID"),
-        os.getenv("JSCHAN_WEBSITE"),
-        allowed_boards=os.getenv("BOARDS_WHITELIST").split() if "BOARDS_WHITELIST" in os.environ else None
-    )
-
-    imageboard = JSChan(os.getenv("JSCHAN_WEBSITE"))
+    mod.start()
 
     if sys.argv[1] == "--purge":
         num_posts = int(sys.argv[2]) if len(sys.argv) > 2 else -1
@@ -39,12 +45,9 @@ if __name__ == "__main__":
             sleep(int(os.getenv("UPDATE_INTERVAL")))
 
     elif sys.argv[1] == "--post":
-        thread_board = sys.argv[2]
-        thread_id = sys.argv[3]
-
-        thread = imageboard.get_thread(thread_board, thread_id)
-
-        bot.post_thread(thread)
+        thread_board = str(sys.argv[2])
+        thread_id = str(sys.argv[3])
+        bot.post_thread(jschan.get_thread(thread_board, thread_id))
 
     else:
         print("Usage: python main.py [--purge <num_posts>] [--start] [--post <board> <id>]")
--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-from .bot import *
-from .jschan import JSChan
